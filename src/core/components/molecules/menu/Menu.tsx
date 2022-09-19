a7eca037e60e4360c9cedaba44afe15530cd0bc1
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-09-19 06:49:54 +0200

    fix: dark mode fixes

--- src/core/components/molecules/menu/Menu.tsx
+++ src/core/components/molecules/menu/Menu.tsx
@@ -16,7 +16,7 @@ export const Menu: FC = () => {
           showMenu ? 'flex fixed' : 'hidden'
         }`}
       >
-        <div className='flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row md:justify-end'>
+        <div className='flex-col w-full h-auto overflow-hidden rounded-lg bg-white dark:bg-slate-600 md:bg-transparent md:dark:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row md:justify-end'>
           <Logo className='ml-5 md:hidden' />
           <div className='flex flex-col items-start justify-center w-full space-x-6 mb-4 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:mb-0 md:mr-4 md:flex-row md:items-center md:justify-end '>
             <Link href='/eventos/1'>Eventos</Link>
@@ -28,7 +28,7 @@ export const Menu: FC = () => {
         <ThemeModeToggle className='hidden md:flex' />
       </div>
 
-      {showMenu && <ThemeModeToggle className='relative right-5 md:hidden' />}
+      {showMenu && <ThemeModeToggle className='relative right-8 md:hidden' />}
 
       <div
         onClick={() => setShowMenu(!showMenu)}
