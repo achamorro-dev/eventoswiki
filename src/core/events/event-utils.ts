1ac0a61d9f536718a296da4783fbba23e44f4c94
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-09-23 07:15:13 +0200

    fix: events ending in different days

--- src/core/events/event-utils.ts
+++ src/core/events/event-utils.ts
@@ -1,21 +1,21 @@
-import { Datetime } from '../datetime/datetime'
-import type { AstroEvent } from './astro-event'
+import { Datetime } from "../datetime/datetime";
+import type { AstroEvent } from "./astro-event";
 
 export class EventUtils {
   static getNextEvents(events: AstroEvent[]): AstroEvent[] {
     return events.filter((event) => {
-      if (!event.frontmatter.startDate) return false
+      if (!event.frontmatter.endDate) return false;
 
-      return Datetime.isAfterYesterday(event.frontmatter.startDate)
-    })
+      return Datetime.isAfterYesterday(event.frontmatter.endDate);
+    });
   }
 
   static getLastEvents(events: AstroEvent[]): AstroEvent[] {
     return events.filter((event) => {
-      if (!event.frontmatter.startDate) return false
+      if (!event.frontmatter.endDate) return false;
 
-      return Datetime.isBeforeToday(event.frontmatter.startDate)
-    })
+      return Datetime.isBeforeToday(event.frontmatter.endDate);
+    });
   }
 
   static sortByStartDateAsc(events: AstroEvent[]): AstroEvent[] {
@@ -23,8 +23,8 @@ export class EventUtils {
       return Datetime.sortByDateAsc(
         eventA.frontmatter.startDate,
         eventB.frontmatter.startDate
-      )
-    })
+      );
+    });
   }
 
   static sortByStartDateDesc(events: AstroEvent[]): AstroEvent[] {
@@ -32,7 +32,7 @@ export class EventUtils {
       return Datetime.sortByDateDesc(
         eventA.frontmatter.startDate,
         eventB.frontmatter.startDate
-      )
-    })
+      );
+    });
   }
 }
