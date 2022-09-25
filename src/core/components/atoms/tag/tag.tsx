0f83f5423ba224f2da0c9c94f20dc26c0c07a6fe
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-09-25 08:08:11 +0200

    feat: increase readability for Tag component

--- src/core/components/atoms/tag/tag.tsx
+++ src/core/components/atoms/tag/tag.tsx
@@ -6,7 +6,7 @@ export const Tag: FC<PropsWithChildren<{ color?: string }>> = ({
 }) => {
   return (
     <span
-      className={`flex items-center px-3 py-1.5 leading-none w-auto rounded-full text-xs font-medium uppercase text-white ${
+      className={`flex items-center px-3 py-1.5 leading-none w-auto rounded-full text-xs font-bold uppercase text-white ${
         !color && "bg-accent"
       }`}
       style={{
