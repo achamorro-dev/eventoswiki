a8e7de912391dfc03c8a683d3872e341a76da170
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-10-02 09:34:56 +0200

    fix: button color

--- src/core/components/atoms/button/Button.tsx
+++ src/core/components/atoms/button/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps = {
 };
 
 const BUTTON_DEFAULT_CLASSES =
-  "inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-primary dark:bg-accent border border-transparent rounded-full md:w-auto hover:bg-primary-light dark:hover:bg-accent-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600";
+  "inline-flex items-center justify-center w-full px-8 py-4 font-bold leading-6 text-white bg-primary dark:bg-accent border border-transparent rounded-full md:w-auto hover:bg-primary-light dark:hover:bg-accent-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600";
 
 const BUTTON_VARIANT_CLASSES: { [variant in ButtonVariant]: string } = {
   outline:
