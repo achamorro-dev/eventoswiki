b10c09258a70d78746da1830f37a3d4ab3d13519
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-03-10 11:21:43 +0100

    feat: add MiduFest 2023

--- src/core/components/molecules/event-date-period/event-date-period.tsx
+++ src/core/components/molecules/event-date-period/event-date-period.tsx
@@ -6,20 +6,31 @@ export const EventDatePeriod: FC<{ startDate: string; endDate?: string }> = ({
   startDate,
   endDate,
 }) => {
+  const startDateHumanized = Datetime.toDateTimeString(startDate);
+  const endDateHumanized = Datetime.toDateTimeString(endDate);
+
   return (
     <div className="flex">
       {startDate && (
         <div className="flex items-center gap-1">
           <Calendar color="white" size="1.2rem" />
-          <p className="text-sm text-white py-2">
-            {Datetime.toDateTimeString(startDate)}
+          <p
+            className="text-sm text-white py-2"
+            aria-label={`Fecha de inicio ${startDateHumanized}`}
+            tabIndex={0}
+          >
+            {startDateHumanized}
           </p>
         </div>
       )}
       {endDate && (
         <div className="flex items-center">
-          <p className="text-sm text-white py-2">
-            &nbsp;· {Datetime.toDateTimeString(endDate)}
+          <p
+            className="text-sm text-white py-2"
+            tabIndex={0}
+            aria-label={`Fecha de fin ${endDateHumanized}`}
+          >
+            &nbsp;· {endDateHumanized}
           </p>
         </div>
       )}
--- src/core/components/molecules/social-link/social-link.tsx
+++ src/core/components/molecules/social-link/social-link.tsx
@@ -11,6 +11,7 @@ import { Youtube } from "../../atoms/icons/youtube";
 type SocialLinkProps = {
   href: string;
   type: SocialNetwork;
+  name: string;
 };
 
 const SocialLinkIcon: { [key in SocialNetwork]: ReactNode } = {
@@ -24,13 +25,14 @@ const SocialLinkIcon: { [key in SocialNetwork]: ReactNode } = {
 };
 
 export const SocialLink: FC<SocialLinkProps> = (props) => {
-  const { href, type } = props;
+  const { href, type, name } = props;
   return (
     <a
       href={href}
       target="_blank"
       className="w-10 h-10 rounded-full flex justify-center items-center bg-white border border-gray-400 shadow-xl"
       rel="nofollow"
+      aria-label={`Enlace a ${type} de ${name}`}
     >
       {SocialLinkIcon[type]}
     </a>
