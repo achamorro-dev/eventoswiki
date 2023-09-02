5b382e23b2fdf8b1f55b232eec1bf91120d5230c
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-09-02 09:23:50 +0200

    feat: update astro, tailwind, react, react-big-calendar and remove tailwind-line-clamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
 
