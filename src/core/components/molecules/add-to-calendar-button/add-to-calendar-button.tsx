a42e16260ba5fea65eb593a62c36876b7ce11d2f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2023-09-05 10:26:35 +0200

    fix: add to calendar button

--- src/core/components/molecules/add-to-calendar-button/add-to-calendar-button.tsx
+++ src/core/components/molecules/add-to-calendar-button/add-to-calendar-button.tsx
@@ -1,19 +1,32 @@
-import { AddToCalendarButton as AddToCalendar } from 'add-to-calendar-button-react';
-import { type FC } from "react";
-import "./add-to-calendar-button.css";
+import { AddToCalendarButton as AddToCalendar } from 'add-to-calendar-button-react'
+import { type FC } from 'react'
+import './add-to-calendar-button.css'
 
 type AddToCalendarButtonProps = {
-  title: string;
-  description?: string;
-  location?: string;
-  startDate: string;
-  endDate: string;
-  startTime: string;
-  endTime: string;
-};
+  title: string
+  description?: string
+  location?: string
+  startDate: string
+  endDate: string
+  startTime: string
+  endTime: string
+}
 
-export const AddToCalendarButton: FC<AddToCalendarButtonProps> = (props) => {
+export const AddToCalendarButton: FC<AddToCalendarButtonProps> = props => {
+  const { title, ...restOfProps } = props
+
   return (
-    <AddToCalendar {...props} listStyle='dropdown' trigger='click' iCalFileName={props.title} label='Añadir a calendario' options={["Apple", "Google", "iCal", "Microsoft365", "Outlook.com"]} timeZone='Europe/Madrid' />
-  );
-};
+    <AddToCalendar
+      name={title}
+      listStyle="dropdown"
+      trigger="click"
+      iCalFileName={title}
+      label="Añadir a calendario"
+      options={['Apple', 'Google', 'iCal', 'Microsoft365', 'Outlook.com']}
+      timeZone="Europe/Madrid"
+      buttonStyle="round"
+      size="4"
+      {...restOfProps}
+    />
+  )
+}
