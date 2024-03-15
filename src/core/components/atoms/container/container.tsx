e1e5e6a147280f2180eb941676eb1f0046c87dbf
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-03-15 08:12:15 +0100

    feat: change base theme

--- src/core/components/atoms/container/container.tsx
+++ src/core/components/atoms/container/container.tsx
@@ -1,23 +1,18 @@
-import type { FC, PropsWithChildren } from "react";
+import type { FC, PropsWithChildren } from 'react'
 type ContainerProps = {
-  maxSize?: "s" | "m" | "l";
-};
+  maxSize?: 's' | 'm' | 'l'
+}
 
 const CONTAINER_MAX_SIZES: { [key: string]: string } = {
-  s: "max-w-3xl",
-  m: "max-w-5xl",
-  l: "max-w-7xl",
-};
+  s: 'max-w-3xl',
+  m: 'max-w-5xl',
+  l: 'max-w-7xl',
+}
 
-export const Container: FC<PropsWithChildren<ContainerProps>> = ({
-  maxSize = "l",
-  children,
-}) => {
+export const Container: FC<PropsWithChildren<ContainerProps>> = ({ maxSize = 'l', children }) => {
   return (
-    <div className="w-full px-6 pb-12 antialiased bg-white dark:bg-slate-800">
-      <div className={`mx-auto ${CONTAINER_MAX_SIZES[maxSize]}`}>
-        {children}
-      </div>
+    <div className="w-full px-6 pb-12 antialiased bg-white dark:bg-slate-900">
+      <div className={`mx-auto ${CONTAINER_MAX_SIZES[maxSize]}`}>{children}</div>
     </div>
-  );
-};
+  )
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,22 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,astro}"],
-  darkMode: "class",
+  content: ['./src/**/*.{js,ts,jsx,tsx,astro}'],
+  darkMode: 'class',
   theme: {
     colors: {
       ...colors,
       primary: {
-        light: colors.indigo[300],
-        DEFAULT: colors.indigo[700],
-        dark: colors.indigo[900],
+        light: colors.rose[500],
+        DEFAULT: colors.rose[600],
+        dark: colors.rose[800],
       },
       accent: {
         light: colors.rose[400],
-        DEFAULT: colors.rose[600],
+        DEFAULT: colors.rose[900],
         dark: colors.rose[800],
       },
     },
   },
-};
+}
