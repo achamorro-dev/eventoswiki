4a6db4337191720b6c365dee452aca03d1ff3194
Cristian Adán <salteadorneo@users.noreply.github.com>
2024-03-18 20:17:08 +0000

    Refactor

--- src/core/components/molecules/tags/tags.tsx
+++ src/core/components/molecules/tags/tags.tsx
@@ -5,20 +5,20 @@ import { EventUtils } from '../../../events/event-utils'
 
 type Props = {
     url: string,
-    tagsEvents: string[]
+    tags: string[]
 }
 
-export const Tags: FC<Props> = ({ url, tagsEvents }) => {
+export const Tags: FC<Props> = ({ url, tags }) => {
     const searchParams = new URL(url).searchParams
-    const tags = searchParams.getAll('tag')
+    const selected = searchParams.getAll('tag')
 
     return (
         <section className="flex flex-wrap gap-1 mb-8">
             {
-                tagsEvents.map(tag => (
+                tags.map(tag => (
                     <a
                         href={EventUtils.getLink(new URL(url), tag)}
-                        className={`inline-flex items-center px-3 py-1.5 leading-none w-auto rounded-full text-xs font-bold uppercase border hover:bg-gray-100 dark:text-white dark:hover:text-slate-900 ${tags.includes(tag) && '!bg-primary !text-white border-primary'}`}
+                        className={`inline-flex items-center px-3 py-1.5 leading-none w-auto rounded-full text-xs font-bold uppercase border hover:bg-gray-100 dark:text-white dark:hover:text-slate-900 ${selected.includes(tag) && '!bg-primary !text-white border-primary'}`}
                     >
                         {tag}
                     </a>
