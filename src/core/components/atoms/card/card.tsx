0652f9d05eae112fe5172f4b30302aaabdb014c6
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-03-21 10:33:41 +0100

    revert: background color on dark mode for Card component

--- src/core/components/atoms/card/card.tsx
+++ src/core/components/atoms/card/card.tsx
@@ -10,7 +10,7 @@ export const Card: FC<PropsWithChildren<CardProps>> = props => {
   return (
     <div
       className={classnames(
-        `col-span-12 flex flex-col items-start overflow-hidden rounded-xl border shadow-sm dark:border-white dark:border-opacity-10 dark:bg-slate-700 md:col-span-6 lg:col-span-4`,
+        `col-span-12 flex flex-col items-start overflow-hidden rounded-xl border shadow-sm dark:border-white dark:border-opacity-10 dark:bg-slate-800 md:col-span-6 lg:col-span-4`,
         className,
       )}
     >
