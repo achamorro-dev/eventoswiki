4d5564f8fafd6bf2269bd98056ca653672d4045d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-05-24 10:34:14 +0200

    fix: event period shrink

--- src/core/components/molecules/event-date-period/event-date-period.tsx
+++ src/core/components/molecules/event-date-period/event-date-period.tsx
@@ -4,12 +4,12 @@ import { Calendar } from '../../../ui/icons'
 
 export const EventDatePeriod: FC<{ startDate: string; endDate?: string }> = ({ startDate, endDate }) => {
   const startDateHumanized = Datetime.toDateTimeString(startDate)
-  const endDateHumanized = Datetime.toDateTimeString(endDate)
+  const endDateHumanized = endDate && Datetime.toDateTimeString(endDate)
 
   return (
     <div className="flex text-black dark:text-white">
       {startDate && (
-        <div className="flex items-center gap-1">
+        <div className="flex shrink-0 items-center gap-1">
           <Calendar size="1.2rem" />
           <p className="py-2 text-sm" aria-label={`Fecha de inicio ${startDateHumanized}`} tabIndex={0}>
             {startDateHumanized}
@@ -17,7 +17,7 @@ export const EventDatePeriod: FC<{ startDate: string; endDate?: string }> = ({ s
         </div>
       )}
       {endDate && (
-        <div className="flex items-center">
+        <div className="flex shrink-0 items-center">
           <p className="py-2 text-sm" tabIndex={0} aria-label={`Fecha de fin ${endDateHumanized}`}>
             &nbsp;· {endDateHumanized}
           </p>
