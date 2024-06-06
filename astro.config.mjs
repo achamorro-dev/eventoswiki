f1705ddd1a8b9fd75201cac904ba8981b0db68ef
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-06-06 07:15:19 +0200

    feat: move to cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
+
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eventos.wiki',
   output: 'server',
-  integrations: [
-    tailwind(),
-    react(),
-    mdx(),
-    vercel({
-      webAnalytics: {
-        enabled: true,
-      },
-    }),
-  ],
+  integrations: [tailwind(), react(), mdx()],
+  adapter: cloudflare(),
 })
