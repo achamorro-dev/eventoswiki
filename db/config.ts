3d598f32f1cdef82784e68c521273b522a643624
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-09-21 11:12:45 +0200

    feat(events): add event table to config

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,36 @@
-import { defineDb } from 'astro:db'
+import { NOW, column, defineDb, defineTable } from 'astro:db'
 
-// https://astro.build/db/config
+const Event = defineTable({
+  columns: {
+    slug: column.text({ primaryKey: true }),
+    title: column.text(),
+    shortDescription: column.text(),
+    startsAt: column.date(),
+    endsAt: column.date(),
+    thumbnail: column.text(),
+    image: column.text(),
+    location: column.text(),
+    web: column.text(),
+    twitter: column.text({ optional: true }),
+    linkedin: column.text({ optional: true }),
+    youtube: column.text({ optional: true }),
+    twitch: column.text({ optional: true }),
+    facebook: column.text({ optional: true }),
+    instagram: column.text({ optional: true }),
+    github: column.text({ optional: true }),
+    telegram: column.text({ optional: true }),
+    whatsapp: column.text({ optional: true }),
+    discord: column.text({ optional: true }),
+    tags: column.text(),
+    tagColor: column.text(),
+    createdAt: column.date({ default: NOW }),
+    updatedAt: column.date({ default: NOW }),
+    content: column.text(),
+  },
+})
+
 export default defineDb({
-  tables: {},
+  tables: {
+    Event,
+  },
 })
