9fc7edd14c8b4874547956e0ad040aab94626d67
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-09-21 11:36:57 +0200

    feat(shared): add Primitives type

---
+++ src/modules/shared/domain/primitives/primitives.ts
@@ -0,0 +1,35 @@
+/**
+ * Review and give an start https://github.com/CodelyTV/typescript-primitives-type
+ *
+ * We work with Date as a string to allow serialization and deserialization
+ */
+/* eslint-disable @typescript-eslint/ban-types */
+type Methods<T> = {
+  [P in keyof T]: T[P] extends Function ? P : never
+}[keyof T]
+
+type MethodsAndProperties<T> = { [key in keyof T]: T[key] }
+
+type Properties<T> = Omit<MethodsAndProperties<T>, Methods<T>>
+
+type PrimitiveTypes = string | number | boolean | undefined | null
+
+type ValueObjectValue<T> = T extends PrimitiveTypes
+  ? T
+  : T extends Date
+    ? string
+    : T extends { value: infer U }
+      ? U
+      : T extends Array<{ value: infer U }>
+        ? U[]
+        : T extends Array<infer U>
+          ? Array<ValueObjectValue<U>>
+          : T extends { [K in keyof Properties<T>]: unknown }
+            ? { [K in keyof Properties<T>]: ValueObjectValue<Properties<T>[K]> }
+            : T extends unknown
+              ? T
+              : never
+
+export type Primitives<T> = {
+  [key in keyof Properties<T>]: ValueObjectValue<T[key]>
+}
