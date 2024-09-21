65d274749938d194908973d7bad2b8aec265afbf
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-09-21 10:49:24 +0200

    feat: migrate tailwind config to extend the default theme properly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,39 +5,40 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,astro}'],
   darkMode: 'class',
   theme: {
-    colors: {
-      ...colors,
-      primary: {
-        light: colors.rose[500],
-        DEFAULT: colors.rose[600],
-        dark: colors.rose[800],
-        foreground: colors.white,
-      },
-      accent: {
-        light: colors.rose[400],
-        DEFAULT: colors.rose[900],
-        dark: colors.rose[800],
-        foreground: colors.white,
-      },
-      background: {
-        light: colors.gray[100],
-        DEFAULT: colors.white,
-        dark: {
-          DEFAULT: colors.slate[900],
-          light: colors.slate[800],
-        },
-      },
-      foreground: {
-        DEFAULT: colors.black,
-        dark: colors.white,
-      },
-      menu: {
-        background: colors.zinc[100],
-        dark: {
-          DEFAULT: colors.slate[800],
+    extend: {
+      colors: {
+        primary: {
+          light: colors.rose[500],
+          DEFAULT: colors.rose[600],
+          dark: colors.rose[800],
           foreground: colors.white,
         },
-        foreground: colors.black,
+        accent: {
+          light: colors.rose[400],
+          DEFAULT: colors.rose[900],
+          dark: colors.rose[800],
+          foreground: colors.white,
+        },
+        background: {
+          light: colors.gray[100],
+          DEFAULT: colors.white,
+          dark: {
+            DEFAULT: colors.slate[900],
+            light: colors.slate[800],
+          },
+        },
+        foreground: {
+          DEFAULT: colors.black,
+          dark: colors.white,
+        },
+        menu: {
+          background: colors.zinc[100],
+          dark: {
+            DEFAULT: colors.slate[800],
+            foreground: colors.white,
+          },
+          foreground: colors.black,
+        },
       },
     },
   },
