726020b24c5a9887029c1ede3b4916f57c641fdf
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-10-03 06:32:26 +0200

    fix: merge conflicts with pnpm-lock and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+import db from '@astrojs/db'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
-
-import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,12 +12,12 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
+    db(),
     mdx(),
     vercel({
       webAnalytics: {
         enabled: true,
       },
     }),
-    db(),
   ],
 })
