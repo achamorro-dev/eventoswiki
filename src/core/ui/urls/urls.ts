496276317b97926db60dfec007821d8785093ea0
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-10-06 19:29:18 +0200

    feat: add province search params for meetups and events pages

--- src/core/ui/urls/urls.ts
+++ src/core/ui/urls/urls.ts
@@ -13,10 +13,14 @@ export const Urls = {
 export const NewUrls = {
   HOME: '/',
   EVENTS: '/eventos/',
+  EVENTS_WITH_PROVINCE: (province: string) => `${NewUrls.EVENTS}?province=${province}`,
   MEETUPS: '/meetups/',
+  MEETUPS_WITH_PROVINCE: (province: string) => `${NewUrls.MEETUPS}?province=${province}`,
   EVENT: (slug: Slug) => `/eventos/${slug}`,
   MEETUP: (slug: Slug) => `/meetups/${slug}`,
   PAST_EVENTS: '/eventos/pasados/',
+  PAST_EVENTS_WITH_PROVINCE: (province: string) => `${NewUrls.PAST_EVENTS}?province=${province}`,
   PAST_MEETUPS: '/meetups/pasados/',
+  PAST_MEETUPS_WITH_PROVINCE: (province: string) => `${NewUrls.PAST_MEETUPS}?province=${province}`,
   PROVINCE: (slug: Slug) => `/${slug}`,
 }
