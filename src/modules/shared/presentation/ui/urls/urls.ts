8e6b997715febe00f2b50bab90cbcf29ada183e9
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-10-09 08:01:24 +0200

    feat(ui): remove the old version of urls

---
+++ src/modules/shared/presentation/ui/urls/urls.ts
@@ -0,0 +1,17 @@
+import type { Slug } from '@/shared/domain/types/slug'
+
+export const Urls = {
+  HOME: '/',
+  EVENTS: '/eventos/',
+  EVENTS_WITH_PROVINCE: (province: string) => `${Urls.EVENTS}?province=${province}`,
+  MEETUPS: '/meetups/',
+  MEETUPS_WITH_PROVINCE: (province: string) => `${Urls.MEETUPS}?province=${province}`,
+  EVENT: (slug: Slug) => `/eventos/${slug}`,
+  MEETUP: (slug: Slug) => `/meetups/${slug}`,
+  PAST_EVENTS: '/eventos/pasados/',
+  PAST_EVENTS_WITH_PROVINCE: (province: string) => `${Urls.PAST_EVENTS}?province=${province}`,
+  PAST_MEETUPS: '/meetups/pasados/',
+  PAST_MEETUPS_WITH_PROVINCE: (province: string) => `${Urls.PAST_MEETUPS}?province=${province}`,
+  PROVINCE: (slug: Slug) => `/${slug}`,
+  CALENDAR: '/calendario',
+}
