e22fced91667e5cd3f269cb7a11db7bff7297abd
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-10-28 06:28:55 +0100

    fix(provinces): order by slug

--- src/modules/provinces/infrastructure/astro-db-provinces.repository.ts
+++ src/modules/provinces/infrastructure/astro-db-provinces.repository.ts
@@ -4,12 +4,12 @@ import type { ProvincesRepository } from '../domain/provinces.repository'
 
 export class AstroDbProvincesRepository implements ProvincesRepository {
   async findAll(): Promise<Array<ProvinceEntity>> {
-    const provinces = await db.select().from(Province).orderBy(Province.name)
+    const provinces = await db.select().from(Province).orderBy(Province.slug)
     return provinces.map(ProvinceEntity.fromPrimitives)
   }
 
   async find(id: ProvinceEntity['slug']): Promise<ProvinceEntity | null> {
-    const provinces = await db.select().from(Province).where(eq(Province.slug, id)).orderBy(Province.name)
+    const provinces = await db.select().from(Province).where(eq(Province.slug, id))
 
     if (!provinces.length) {
       return null
