587ad56457a9cb07238d8a41a4f49dd712247d99
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-05-24 07:28:26 +0200

    feat: add DateValidator and DateFormField

---
+++ src/modules/shared/presentation/forms/date-form-field.ts
@@ -0,0 +1,9 @@
+import type { ValidatorConstructor } from '@/shared/domain/validators/validator'
+import { z } from 'astro/zod'
+import { validateField } from './validate-field'
+
+export const DateFormField = <Value>(validator: ValidatorConstructor<Value>) => {
+  return z
+    .date({ required_error: 'Este campo es obligatorio' })
+    .superRefine((value, context) => validateField<Value>(value as Value, context, validator))
+}
