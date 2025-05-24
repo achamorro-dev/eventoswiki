46f8394d43816663201677ad68d32d198c7e0cd4
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-05-24 07:48:00 +0200

    feat: remove exampleContent for RichEditor

--- src/modules/shared/presentation/ui/components/rich-editor/rich-editor.tsx
+++ src/modules/shared/presentation/ui/components/rich-editor/rich-editor.tsx
@@ -27,46 +27,15 @@ const extensions = [
   }),
 ]
 
-const exampleContent = `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That’s a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that’s amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
-`
-
 interface RichEditorProps {
   content?: string
   onContentChange: (content: string) => void
 }
 export const RichEditor = (props: RichEditorProps) => {
-  const { content = exampleContent, onContentChange } = props
+  const { content, onContentChange } = props
 
   return (
-    <div className="border-input rounded border">
+    <div className="border-input w-full rounded border">
       <EditorProvider
         slotBefore={<RichEditorToolbar />}
         extensions={extensions}
