1330cdad53613fd13d54da5c3c09f24655b058cd
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-05-24 07:07:31 +0200

    fix: ref on shadcn button component

--- src/modules/shared/presentation/ui/button.tsx
+++ src/modules/shared/presentation/ui/button.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/modules/shared/presentation/ui/lib/utils'
 
@@ -32,19 +32,18 @@ const buttonVariants = cva(
   },
 )
 
-function Button({
-  className,
-  variant,
-  size,
-  asChild = false,
-  ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithRef<'button'> &
+    VariantProps<typeof buttonVariants> & {
+      asChild?: boolean
+    }
+>(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : 'button'
 
-  return <Comp data-slot="button" className={cn(buttonVariants({ variant, size, className }))} {...props} />
-}
+  return <Comp data-slot="button" className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+})
+
+Button.displayName = 'Button'
 
 export { Button, buttonVariants }
