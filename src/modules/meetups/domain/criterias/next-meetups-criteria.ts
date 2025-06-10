cb1946d38f22452cb7d71e9c874142a82bc76a3f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-06-10 06:13:11 +0200

    fix: next meetups criteria taking properly started meetups

--- src/modules/meetups/domain/criterias/next-meetups-criteria.ts
+++ src/modules/meetups/domain/criterias/next-meetups-criteria.ts
@@ -3,23 +3,26 @@ import { Filter } from '@/shared/domain/criteria/filter'
 import { RelationalOperator } from '@/shared/domain/criteria/relational-operator'
 import { Datetime } from '@/shared/domain/datetime/datetime'
 import { MeetupsCriteria } from './meetups-criteria'
+import type { MeetupsFilters } from './meetups-filters'
 
 export class NextMeetupsCriteria extends MeetupsCriteria {
-  static createWith({ location }: { location?: string }): NextMeetupsCriteria {
+  static createWith(filters: { location?: string }): NextMeetupsCriteria {
     const now = Datetime.now()
 
     return MeetupsCriteria.create()
       .orderBy({ startsAt: OrderDirection.ASC, endsAt: OrderDirection.ASC })
       .and([
-        Filter.or([
+        Filter.or<Partial<MeetupsFilters>>([
           Filter.and({
             startsAt: { operator: RelationalOperator.GREATER_THAN_OR_EQUAL, value: now },
             endsAt: { operator: RelationalOperator.LOWER_THAN, value: now },
           }),
-          Filter.and({ startsAt: { operator: RelationalOperator.GREATER_THAN_OR_EQUAL, value: now } }),
+          Filter.and({
+            endsAt: { operator: RelationalOperator.GREATER_THAN_OR_EQUAL, value: now },
+          }),
         ]),
-        Filter.and({
-          location: location ? { operator: RelationalOperator.EQUALS, value: location } : undefined,
+        Filter.and<Partial<MeetupsFilters>>({
+          location: filters.location ? { operator: RelationalOperator.EQUALS, value: filters.location } : undefined,
         }),
       ])
   }
