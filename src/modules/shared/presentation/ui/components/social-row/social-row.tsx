e1bb1dc7f9f1074d8ab1326f1ee45921d290143d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-06-18 06:45:36 +0200

    fix: social row items position in organization page

--- src/modules/shared/presentation/ui/components/social-row/social-row.tsx
+++ src/modules/shared/presentation/ui/components/social-row/social-row.tsx
@@ -1,5 +1,6 @@
 import { SocialNetwork } from '@/shared/domain/social/social-network'
 import { SocialLink } from '../social-link/social-link'
+import { cn } from '@/ui/lib/utils'
 
 interface Props {
   name: string
@@ -14,13 +15,27 @@ interface Props {
   whatsapp?: string
   discord?: string
   web?: string
+  className?: string
 }
 
 export const SocialRow = (props: Props) => {
-  const { twitter, twitch, instagram, youtube, facebook, linkedin, github, telegram, whatsapp, discord, web } = props
+  const {
+    twitter,
+    twitch,
+    instagram,
+    youtube,
+    facebook,
+    linkedin,
+    github,
+    telegram,
+    whatsapp,
+    discord,
+    web,
+    className,
+  } = props
 
   return (
-    <div className="left-0 right-0 -ml-2 flex w-full justify-start gap-1 xl:px-0">
+    <div className={cn('left-0 right-0 -ml-2 flex w-full justify-start gap-1 xl:px-0', className)}>
       {web && <SocialLink name={web} href={web} type={SocialNetwork.web} />}
       {twitter && <SocialLink name={twitter} href={twitter} type={SocialNetwork.twitter} />}
       {twitch && <SocialLink name={twitch} href={twitch} type={SocialNetwork.twitch} />}
