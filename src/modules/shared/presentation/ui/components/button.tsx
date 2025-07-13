18ec2ed0d8f9c212f6991685d16895d0c1f9985d
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-07-13 08:37:08 +0200

    feat: minor changes for links

--- src/modules/shared/presentation/ui/components/button.tsx
+++ src/modules/shared/presentation/ui/components/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 'text-muted-foreground hover:text-primary hover:no-underline',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
