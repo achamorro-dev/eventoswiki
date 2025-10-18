ef3604b51542313fc05bde4fe6e180f3cfa50ae4
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-10-18 11:32:36 +0200

    fix: queries for find events with multiple filters

--- src/modules/events/application/find-events.query.ts
+++ src/modules/events/application/find-events.query.ts
@@ -1,6 +1,7 @@
 import { Query } from '@/shared/application/use-case/query'
 import { OrderDirection } from '@/shared/domain/criteria/order-direction'
 import type { PaginatedResult } from '@/shared/domain/criteria/paginated-result'
+import { RelationalOperator } from '@/shared/domain/criteria/relational-operator'
 import { EventsCriteria } from '../domain/criterias/events-criteria'
 import type { Event } from '../domain/event'
 import type { EventsRepository } from '../domain/events.repository'
@@ -23,9 +24,12 @@ export class FindEventsQuery extends Query<PaginatedResult<Event>, FindEventsReq
 
     const criteria = EventsCriteria.create()
       .orderBy({ startsAt: OrderDirection.DESC })
-      .withOrganizationId(organizationId)
-      .withStartsAndEndsAt(startsAt, endsAt)
-      .withLocation(province)
+      .and({
+        organizationId: organizationId ? { operator: RelationalOperator.EQUALS, value: organizationId } : undefined,
+        startsAt: startsAt ? { operator: RelationalOperator.GREATER_THAN_OR_EQUAL, value: startsAt } : undefined,
+        endsAt: endsAt ? { operator: RelationalOperator.LOWER_THAN, value: endsAt } : undefined,
+        location: province ? { operator: RelationalOperator.EQUALS, value: province } : undefined,
+      })
       .withLimit(limit)
 
     return this.eventRepository.match(criteria)
--- src/modules/meetups/application/find-meetups.query.ts
+++ src/modules/meetups/application/find-meetups.query.ts
@@ -1,6 +1,7 @@
 import { Query } from '@/shared/application/use-case/query'
 import { OrderDirection } from '@/shared/domain/criteria/order-direction'
 import { PaginatedResult } from '@/shared/domain/criteria/paginated-result'
+import { RelationalOperator } from '@/shared/domain/criteria/relational-operator'
 import { MeetupsCriteria } from '../domain/criterias/meetups-criteria'
 import type { Meetup } from '../domain/meetup'
 import type { MeetupsRepository } from '../domain/meetups.repository'
@@ -23,9 +24,12 @@ export class FindMeetupsQuery extends Query<PaginatedResult<Meetup>, FindMeetups
 
     const criteria = MeetupsCriteria.create()
       .orderBy({ startsAt: OrderDirection.DESC })
-      .withOrganizationId(organizationId)
-      .withStartsAndEndsAt(startsAt, endsAt)
-      .withLocation(province)
+      .and({
+        organizationId: organizationId ? { operator: RelationalOperator.EQUALS, value: organizationId } : undefined,
+        startsAt: startsAt ? { operator: RelationalOperator.GREATER_THAN_OR_EQUAL, value: startsAt } : undefined,
+        endsAt: endsAt ? { operator: RelationalOperator.LOWER_THAN, value: endsAt } : undefined,
+        location: province ? { operator: RelationalOperator.EQUALS, value: province } : undefined,
+      })
       .withLimit(limit)
 
     return this.meetupRepository.match(criteria)
