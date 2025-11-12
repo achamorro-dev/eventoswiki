c4a955871fb8b6e7f4ca60deddc439e54f5cc310
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2025-11-12 18:06:37 +0100

    feat: reduce font size and increase font weight for section titles

--- src/modules/shared/presentation/ui/components/section-title/section-title.tsx
+++ src/modules/shared/presentation/ui/components/section-title/section-title.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react'
 
 export const SectionTitle = ({ children }: PropsWithChildren) => {
-  return <h2 className="text-primary mb-4 text-4xl font-medium leading-none lg:text-5xl">{children}</h2>
+  return <h2 className="text-primary mb-4 text-3xl font-semibold leading-none">{children}</h2>
 }
